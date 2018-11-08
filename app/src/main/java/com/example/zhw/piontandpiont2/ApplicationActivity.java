2e4be8c9344921532ea516da4450f6aa5cab3499
13674005942 <1363111351@qq.com>
2018-11-08 15:12:02 +0800

    2

--- app/src/main/java/com/example/zhw/piontandpiont2/ApplicationActivity.java
+++ app/src/main/java/com/example/zhw/piontandpiont2/ApplicationActivity.java
@@ -1,16 +1,15 @@
 package com.example.zhw.piontandpiont2;
 
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.zhw.piontandpiont2.Util.BaseActivity;
 
 public class ApplicationActivity extends BaseActivity implements View.OnClickListener {
     public ImageView message_image_back;
+    private Button btn_accept,btn_decline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,17 +19,30 @@ public class ApplicationActivity extends BaseActivity implements View.OnClickLis
     }
 
     private void initView() {
-            message_image_back = findViewById(R.id.image_message_back);
-            message_image_back.setOnClickListener(this);
+        message_image_back = findViewById(R.id.image_message_back);
+        message_image_back.setOnClickListener(this);
+        btn_accept=findViewById(R.id.application_button_accept);
+        btn_accept.setOnClickListener(this);
+        btn_decline=findViewById(R.id.application_decline);
+        btn_decline.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.image_message_back:  //返回键
-                        Intent homeActivity = new Intent(ApplicationActivity.this, HomeActivity.class);
-                        homeActivity.putExtra("id",2);
-                       startActivity(homeActivity);
+                // Intent homeActivity = new Intent(ApplicationActivity.this, HomeActivity.class);
+                // homeActivity.putExtra("id",2);
+                //  startActivity(homeActivity);
+                this.finish();
+                break;
+            case R.id.application_button_accept:  //接受
+                btn_accept.setText("踢出群");
+                btn_decline.setVisibility(View.GONE);
+                break;
+            case R.id.application_decline:   //拒绝
+                btn_accept.setText("邀请进群");
+                btn_decline.setVisibility(View.GONE);
                 break;
         }
     }
